e1685180b904197b447443ed73b06f6a03cfbcbf
Nabil Ridhwan <nabridhwan@gmail.com>
2021-12-11 16:24:44 +0800

    Home page

--- App.js
+++ App.js
@@ -7,57 +7,151 @@
  */
 
 import React, { useState, useEffect } from 'react';
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList, Image, SafeAreaView, StyleSheet, Text, View, ImageBackground } from "react-native";
 import Theme from './config/Theme';
 
-const path = require("path");
+import HomePageData from "./homePageData"
 
 export default function App() {
 
   const [pages, setPages] = useState(["All", "Singapore", "Oceania", "Asia", "Europe", "America"])
   const [currentPage, setCurrentPage] = useState();
+  const [homePageData, setHomePageData] = useState({});
 
   const handlePageChange = (page) => {
+    console.log(page)
     setCurrentPage(page);
   }
 
   useEffect(() => {
-    setCurrentPage(pages[0]);
+    Object.keys(HomePageData).forEach(region => {
+      HomePageData["All"] = [...HomePageData[region]]
+    })
+
+    setHomePageData(HomePageData)
+    setCurrentPage(pages[1]);
   }, [])
 
+  const renderItem = (guide) => {
+    // console.log(`./images/${guide.image_url}`)
+    return (
+      <View style={styles.card}>
+        <View>
+          <ImageBackground source={guide.image_url} style={card.image} resizeMode='cover'>
+
+
+            <View style={{ flex: 1, justifyContent: "space-between", padding: 10 }}>
+
+              <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
+                <Text style={styles.defaultText}>Top left</Text>
+                <Text style={styles.defaultText}>Top right</Text>
+
+              </View>
+
+              {/* TODO: Fix text wrappings for title and author! */}
+              <View>
+                <Text style={{...styles.defaultText, flexWrap: "wrap"}}>{guide.title}</Text>
+                <Text style={styles.defaultText}>{guide.author}</Text>
+              </View>
+            </View>
+
+          </ImageBackground>
+
+        </View>
+      </View>
+    )
+  }
+
   return (
-    <SafeAreaView style={styles.backgroundStyle}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Image source={require("./images/header.png")} style={styles.headerImage} resizeMode='cover' />
 
-      <Image source={require(__dirname + "/images/Group 68.png")} />
-      
-      <View style={styles.navigation}>
-        {pages.map((page, index) =>
+      <View style={{ flex: 1, ...styles.backgroundStyle }}>
 
-          <View style={styles.innerNavigation} key={index} onTouchEnd={() => handlePageChange(page)}>
-            <Text style={currentPage == page ? styles.highlightedText : styles.defaultText}>
-              {page}
-            </Text>
+        <View style={styles.navigation}>
+          {pages.map((page, index) =>
 
-            <View style={currentPage == page ? styles.navigationCircle : styles.displayHidden}></View>
-          </View>
+            <View style={styles.innerNavigation} key={index} onTouchEnd={() => handlePageChange(page)}>
 
-        )}
+              <Text style={currentPage == page ? styles.highlightedText : styles.defaultText}>
+                {page}
+              </Text>
+
+              <View style={currentPage == page ? styles.navigationCircle : styles.displayHidden}></View>
+            </View>
+
+          )}
+        </View>
+
+        {/* Header part
+        <View>
+          <Text style={styles.title}>Explore new places</Text>
+          <Text style={styles.subtitle}>Travel and explore the world!</Text>
+        </View>
+
+        {/* Main cards */}
+
+        <View style={styles.cardWrapper}>
+          {/* <FlatList data={homePageData[currentPage]} renderItem={renderItem} keyExtractor={item => item.id} /> */}
+          {homePageData[currentPage] &&
+            homePageData[currentPage].map(places => renderItem(places))
+          }
+        </View>
+
+        {/* <View style={{ flexDirection: "row" ,flexWrap: "wrap"}}>
+
+            <View style={{  height: 200,  width: 150, backgroundColor: "red" }}>
+              <View>
+                <Text>Hi</Text>
+                <Text>Hi</Text>
+              </View>
+            </View>
+
+            <View style={{  height: 200,  width: 150, backgroundColor: "purple" }}>
+
+            </View>
+
+            <View style={{  height: 200,  width: 150, backgroundColor: "blue" }}>
+
+            </View>
+
+          </View> */}
+
       </View>
 
-      <View>
-        <Text style={styles.title}>Explore new places</Text>
-        <Text style={styles.subtitle}>Travel and explore the world!</Text>
-      </View>
-      
     </SafeAreaView>
   );
 };
 
+const card = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 200,
+  }
+})
+
 const styles = StyleSheet.create({
+
+  card: {
+    width: "45%",
+    height: "auto",
+    marginBottom: 20,
+    borderRadius: 400,
+  },
+
+  cardWrapper: {
+    flex: 1,
+    flexWrap: "wrap",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  headerImage: {
+    width: "100%",
+  },
+
   backgroundStyle: {
     padding: 20,
-    flex: 1,
-    backgroundColor: Theme.backgroundColor
+    backgroundColor: Theme.backgroundColor,
   },
 
   defaultText: {
---
+++ homePageData.js
@@ -0,0 +1,61 @@
+const post = {
+    "Singapore": [{
+            "id": 1,
+            "title": "Universal Studios Singapore's hidden gems",
+            "author": "TheSingaporeTraveller",
+            "image_url": require(`./images/singapore/1.png`)
+        },
+
+
+        {
+            "id": 2,
+
+            "title": "The Adventure Cove experience.",
+            "author": "TheSingaporeTraveller",
+            "image_url": require(`./images/singapore/3.png`)
+        },
+
+        {
+            "id": 3,
+
+            "title": "Best hotel rooms in Marina Bay Sands",
+            "author": "LuxurySGTraveller",
+            "image_url": require(`./images/singapore/2.png`)
+        },
+
+        {
+            "id": 4,
+
+            "title": "Things you should look out for in S.E.A Aquarium",
+            "author": "LimChooK",
+            "image_url": require(`./images/singapore/4.png`)
+        }
+    ],
+
+    "Oceania": [{
+            "id": 5,
+
+            "title": "Places to look out in New Zealand",
+            "author": "NewZealanderTraveller",
+            "image_url": require(`./images/oceania/1.png`)
+        },
+
+        {
+            "id": 6,
+
+            "title": "The Opera House - Picture spots",
+            "author": "PhotoHolicGuy23",
+            "image_url": require(`./images/oceania/2.png`)
+        }
+    ],
+
+    "China": [{
+        "id": 7,
+
+        "title": "Beijing's Forbidden City is 600 years old",
+        "author": "ChinaTraveller 中国",
+        "image_url": require(`./images/china/1.png`)
+    }]
+}
+
+export default post;
