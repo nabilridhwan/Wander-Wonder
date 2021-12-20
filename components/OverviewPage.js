fe0b5512fc3409359c37321c52257536ea337a6a
Savagexr <xronglee.21@ichat.sp.edu.sg>
2021-12-20 15:50:11 +0800

    Added overview page

---
+++ components/OverviewPage.js
@@ -0,0 +1,137 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React, { Component } from 'react';
+ import { ImageBackground, Platform, Image, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+ import SwipeUpDown from 'react-native-swipe-up-down';
+ import Icon from 'react-native-vector-icons/Ionicons';
+ export default class Overview extends Component {
+   constructor(props) {
+     super(props);
+ 
+     this.state = {
+       animation: "easeInEaseOut"
+     }
+   }
+   render() {
+     return (
+       <View style={styles.container}>
+         <ImageBackground source={require('./images/USS.jpg')} style={styles.image}>
+           <View style={styles.smallContainer}>
+             <Icon name="arrow-back-circle" size={45} color="silver" style={{ marginLeft: 7, marginTop: 7 }} />
+             <Icon name="heart" size={45} color="red" style={{ marginRight: 7, marginTop: 7 }} />
+           </View>
+ 
+         </ImageBackground>
+         <SwipeUpDown
+           hasRef={ref => (this.swipeUpDownRef = ref)}
+           itemMini={
+             <View style={{ alignItems: 'center' }}>
+               <Text>This is the mini view, swipe up!</Text>
+             </View>
+           }
+           itemFull={
+             <View style={styles.panelContainer}>
+               <Text style={styles.instructions}>Universal Studio Singapore</Text>
+               <Text style={styles.author}>By TheSingaporeTraveller</Text>
+               <View style={styles.subcontainer}>
+                 <View style={styles.button}>
+                   <Text style={styles.overview}>Overview</Text>
+                 </View>
+                 <Text style={styles.itinerary}>Itinerary</Text>
+               </View>
+               <Text style={styles.singlequote}>“</Text>
+               <Text style={styles.description}>Singapore's only movie-theme park features 24 rides and attractions, including a pair of dueling coasters that brush past one another in several near misses in their aerial combat. Thrill-seekers can choose between a coaster where they are seated or the other where riders are suspended. Exciting loops, sudden turns, breathtaking drops and thrilling near-collisions will make you scream your heart out. Dinosaurs, lemurs, ogres and Egyptian mummies add to the thrills and adventures.With 18 attractions created or redesigned for Singapore, Universal Studios Singapore™ promises an experience that you will not forget. Laughter and screams are heard around every corner, and lasting memories are made every day. Themed dining and shops are available.</Text>
+               <Text style={styles.singlequote}>”</Text>
+             </View>
+           }
+           onShowMini={() => console.log('mini')}
+           onShowFull={() => console.log('full')}
+           disablePressToShow={true}
+           style={{ backgroundColor: 'black', borderTopRightRadius: 30, borderTopLeftRadius: 30, borderTopEndRadius: 30 }}
+           animation={this.state.animation}
+         />
+       </View>
+     );
+   }
+ }
+ 
+ const styles = StyleSheet.create({
+   instructions: {
+     textAlign: 'center',
+     color: 'white',
+     marginTop: 30,
+     marginBottom:20,
+     fontWeight: '900',
+     fontSize: 26
+   },
+   author: {
+     textAlign: 'center',
+     color: '#BFBFBF',
+     marginBottom: 20,
+     fontSize: 14,
+   },
+   subcontainer:{
+     flexDirection:"row",
+     justifyContent:"space-evenly"
+   },
+   overview: {
+     color: 'white',
+     marginBottom: 20,
+     fontWeight: '900',
+     fontSize: 20,
+     textAlign:'center',
+   },
+   button: {
+     backgroundColor: '#8987FF',
+     borderWidth: 1,
+     borderRadius: 32,
+     margin: 5,
+     padding:15,
+     paddingBottom:4
+   },
+   itinerary:{
+     color:'white',
+     marginBottom: 20,
+     marginLeft:15,
+     fontWeight: '900',
+     fontSize: 20,
+     textAlign:'center',
+     paddingTop:20
+   },
+   singlequote:{
+     color:'#FE5252',
+     fontSize:60,
+     padding:0,
+     margin:0
+   },
+   description:{
+     color:'white',
+     fontWeight:'bold',
+     lineHeight:25,
+     margin:8,
+     justifyContent:'space-evenly'
+   },
+   panelContainer: {
+     flex: 1,
+     justifyContent: 'center'
+   },
+   container: {
+     flex: 1,
+   },
+   image: {
+     flex: 1,
+     resizeMode: 'contain'
+   },
+   smallContainer: {
+     flexDirection: 'row',
+     justifyContent: 'space-between'
+   }
+ });
+ 
+ 
