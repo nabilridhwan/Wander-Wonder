c8c2b60d5b5a72a6cf50b0f5ebd3e13eed4dae8a
Savagexr <xronglee.21@ichat.sp.edu.sg>
2021-12-23 00:01:22 +0800

    Update Search.js

--- components/Search.js
+++ components/Search.js
@@ -15,6 +15,7 @@
 import React from 'react';
 import { StyleSheet, View, TouchableOpacity, TextInput } from "react-native";
 import Theme from '../config/Theme';
+import Icon from 'react-native-vector-icons/Ionicons';
 import Guides from "../assets/data/Guides";
 
 export default class Search extends React.Component {
@@ -38,8 +39,9 @@ export default class Search extends React.Component {
     return (
       <View style={{ flex: 1, ...styles.backgroundStyle }}>
 
-           <TouchableOpacity style={{backgroundColor: "rgba(255,255,255,0.3)", height: 45, justifyContent: "center", borderRadius: 10, marginBottom: 10}}>
-                <TextInput placeholder='Search' placeholderTextColor={"rgba(255,255,255,0.6)"} style={{color: "white", padding: 10}} onChangeText={(inputText) => this.handleSearch(inputText)}></TextInput>
+           <TouchableOpacity style={{flexDirection:"row",backgroundColor: "rgba(255,255,255,0.3)", height: 45, justifyContent: "space-around", borderRadius: 10, marginBottom: 10}}>
+                <TextInput placeholder='Search Guide' placeholderTextColor={"rgba(255,255,255,0.6)"} style={{color: "white", padding: 10}} onChangeText={(inputText) => this.handleSearch(inputText)}></TextInput>
+                <Icon onPress={(inputText) => this.handleSearch(inputText)} name="search" color="white" size={26}/>
            </TouchableOpacity>
 
       </View >
