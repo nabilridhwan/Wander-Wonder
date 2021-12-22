0daeb9edaad7bc4bab9f2baa9ff5645b06960abc
Savagexr <xronglee.21@ichat.sp.edu.sg>
2021-12-22 20:43:11 +0800

    Updated Itinerary

--- components/ItineraryContent.js
+++ components/ItineraryContent.js
@@ -13,7 +13,18 @@ import Theme from '../config/Theme';
 export default ({ place }) => {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={{color: Theme.textColor}} >Itinerary Page</Text>
+            <View style={{flexDirection:"column",justifyContent:"flex-start"}}>
+                <Text style={styles.time}>10:30 AM</Text>
+            </View>
+            <View style={{flexDirection:"column",justifyContent:"flex-start"}}>
+                <View style={styles.itinerary}>
+                <Icon name="train" color="black" size={40}/> 
+                </View>
+                <View style={styles.lines}/>
+                {/* <View style={styles.itinerary}>
+                <Icon name="pin" color="black" size={40}/> 
+                </View> */}
+            </View>
         </SafeAreaView>
     );
 }
@@ -22,5 +33,27 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    itinerary:{
+        alignItems:'center',
+        backgroundColor:'#70FFF6',
+        borderRadius:999,
+        marginHorizontal:29,
+        padding:9,
+        width:64,
+        height:64
+    },
+    lines:{
+        backgroundColor:'#70FFF6',
+        position:'absolute',
+        width:18,
+        height:50,
+        top:54,
+        left:54
+    },
+    time:{
+        fontStyle:'italic',
+        fontWeight:'900',
+        color:Theme.textColor
+    }
 });
 
